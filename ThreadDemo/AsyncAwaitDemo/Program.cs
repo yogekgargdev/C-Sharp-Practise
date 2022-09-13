3fe530699b4bddb7b838a1ffc72c77a41ba4ed9e
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2022-09-13 12:37:24 +0530

    AsyncWaitDemo basic

---
+++ ThreadDemo/AsyncAwaitDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.PrintData();
+            Console.ReadKey();
+
+        }
+
+        public void PrintData()
+        {
+            int result = 5;
+            Task<int> t=GetData();
+            result = t.Result;  
+            Console.WriteLine($"Retured Data is: {result}");
+        }
+
+        public Task<int> GetData()
+        {
+            return Task.FromResult(45);
+        }
+
+    }
+}
