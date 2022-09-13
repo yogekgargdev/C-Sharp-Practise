66b53a4a347a0aeb1c8cc1c60766a9ef66cf6c60
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2022-09-13 11:35:36 +0530

    commit

---
+++ ThreadDemo/SyncDemo/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncDemo
+{
+    public class Calculator
+    {
+        public int No1 { get; set; }
+        public int No2 { get; set; }
+        public void Addition()
+        {
+            var result=No1+ No2;
+            Console.WriteLine("Addition is: "+result);
+        }
+        public void Subtraction()
+        {
+            var result = No1 - No2;
+            Console.WriteLine("Subtraction is: " + result);
+        }
+
+        public void Division()
+        {
+            var result = No1/No2;
+            Console.WriteLine("Division is: " + result);
+        }
+
+        public void Multiplacation()
+        {
+            var result = No1 * No2;
+            Console.WriteLine("Multiplication is: " + result);
+        }
+
+    }
+}
--- ThreadDemo/SyncDemo/Program.cs
+++ ThreadDemo/SyncDemo/Program.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SyncDemo
 {
-    internal class Program
+     public class Program
     {
         static void Main(string[] args)
         {
+
+            Calculator c1 = new Calculator() { No1=50,No2=5};
+            Thread t1, t2, t3, t4;
+            t1 = new Thread(c1.Addition);
+            t2 = new Thread(c1.Subtraction);
+            t3 = new Thread(c1.Multiplacation);
+            t4 = new Thread(c1.Division);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+
+            Console.Read();
+
+
         }
     }
 }
