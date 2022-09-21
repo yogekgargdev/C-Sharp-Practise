dd0110579e4946389879177a21c2993f2d8733ff
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2022-09-21 15:14:28 +0530

    Commit

--- ADO.NET/EmployeeDbDemo/EmployeeDbDemo/Program.cs
+++ ADO.NET/EmployeeDbDemo/EmployeeDbDemo/Program.cs
@@ -33,11 +33,13 @@ namespace EmployeeDbDemo
             //string updtStr = $"update Employee set ename='{ename}',gender='{gender}',eaddress='{address}',esalary='{salary}' where eid={eid}";
 
             //string delStr = $"delete from Employee where eid={eid}";
-            string sltSelect = "select * from Employee";
+            //string sltSelect = "select * from Employee";
+            string fltrsltSelect = "select * from Employee where eid=2";
             //SqlCommand cmd = new SqlCommand(inStr,conn);
             //SqlCommand cmd = new SqlCommand(updtStr, conn);
             //SqlCommand cmd = new SqlCommand(delStr, conn);
-            SqlCommand cmd = new SqlCommand(sltSelect, conn);
+            //SqlCommand cmd = new SqlCommand(sltSelect, conn);
+            SqlCommand cmd = new SqlCommand(fltrsltSelect, conn);
             //cmd.CommandText = inStr;
             //cmd.Connection=conn;
 
@@ -53,15 +55,28 @@ namespace EmployeeDbDemo
 
                 dr= cmd.ExecuteReader();    //-> dr acts as pointer
                 Console.WriteLine("ID\tName\tGender\tAddress\t\tSalary");
+                /*
                 Console.WriteLine("------------------------------------------------------");
                 while(dr.Read())
                 {
                     //Console.WriteLine($"{dr.GetInt32(0)}\t{dr.GetString(1)}\t{dr.GetString(2)}\t{dr.GetString(3)}\t\t{dr.GetDouble(4)}"); 
                     //-> 0 stands for 0th Collumn, above thing is also valid
-                    Console.WriteLine($"{dr[0]}\t{dr[1]}\t{dr[2]}\t{dr[3]}\t\t{dr[4]}");
+                    //Console.WriteLine($"{dr[0]}\t{dr[1]}\t{dr[2]}\t{dr[3]}\t\t{dr[4]}"); // this is also valid
+                    Console.WriteLine($"{dr["EID"]}\t{dr["EName"]}\t{dr["Gender"]}\t{dr["EAddress"]}\t\t{dr["ESalary"]}");
                 }
 
-                Console.WriteLine("------------------------------------------------------");
+                       Console.WriteLine("------------------------------------------------------");
+                */
+
+                if (dr.HasRows)
+                {
+                    //-> pointer is on 0 pointer, so increase to 1 otherwise data wont be acccessed
+                    dr.Read();
+                    Console.WriteLine($"{dr["EID"]}\t{dr["EName"]}\t{dr["Gender"]}\t{dr["EAddress"]}\t\t{dr["ESalary"]}\n");
+
+                }
+
+         
                 Console.WriteLine("Data Retrieval Success");
 
             }
