cb8ff8148c04804230c4d857cdf398c355ca5bf2
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2022-09-21 12:21:05 +0530

    update delete and select queries

--- ADO.NET/EmployeeDbDemo/EmployeeDbDemo/Program.cs
+++ ADO.NET/EmployeeDbDemo/EmployeeDbDemo/Program.cs
@@ -17,8 +17,8 @@ namespace EmployeeDbDemo
             int eid = 4;
             string ename = "Raghu";
             string gender = "Male";
-            string address = "Hyedrabad";
-            float salary = 35000.0f;
+            string address = "Bangalore";
+            float salary = 50000.0f;
 
             
             SqlConnection conn = null; // this is a connection object
@@ -28,23 +28,48 @@ namespace EmployeeDbDemo
             // we can give connection details via constructor or via the props of conn
             //conn.ConnectionString="datasource=.;database=SoleraEmployees;user id=sa;pwd=sa" // this thing is also valid
 
-            string inStr = $"insert into Employee values({eid},'{ename}','{gender}','{address}',{salary})";
+            //string inStr = $"insert into Employee values({eid},'{ename}','{gender}','{address}',{salary})";
 
-            SqlCommand cmd = new SqlCommand(inStr,conn);
+            //string updtStr = $"update Employee set ename='{ename}',gender='{gender}',eaddress='{address}',esalary='{salary}' where eid={eid}";
+
+            //string delStr = $"delete from Employee where eid={eid}";
+            string sltSelect = "select * from Employee";
+            //SqlCommand cmd = new SqlCommand(inStr,conn);
+            //SqlCommand cmd = new SqlCommand(updtStr, conn);
+            //SqlCommand cmd = new SqlCommand(delStr, conn);
+            SqlCommand cmd = new SqlCommand(sltSelect, conn);
             //cmd.CommandText = inStr;
             //cmd.Connection=conn;
+
+            SqlDataReader dr = null; 
 
             try
             {
                 conn.Open();
-                cmd.ExecuteNonQuery();   // return the no of rows executed
-                
+                //cmd.ExecuteNonQuery();   // return the no of rows executed - works for insert,update, delete,create 
+                //Console.WriteLine("Record Inserted Success!");
+                //Console.WriteLine("Record Update Success!");
+                //Console.WriteLine("Record Delete Success!");
+
+                dr= cmd.ExecuteReader();    //-> dr acts as pointer
+                Console.WriteLine("ID\tName\tGender\tAddress\t\tSalary");
+                Console.WriteLine("------------------------------------------------------");
+                while(dr.Read())
+                {
+                    Console.WriteLine($"{dr.GetInt32(0)}\t{dr.GetString(1)}\t{dr.GetString(2)}\t{dr.GetString(3)}\t\t{dr.GetDouble(4)}"); 
+                    //-> 0 stands for 0th Collumn
+
+                }
+
+                Console.WriteLine("------------------------------------------------------");
+                Console.WriteLine("Data Retrieval Success");
+
             }
             catch(SqlException ex)
             {
                 Console.WriteLine(ex.Message); 
-                Console.WriteLine(ex.StackTrace);   
-
+                Console.WriteLine(ex.StackTrace);
+          
             }
             finally
             {
