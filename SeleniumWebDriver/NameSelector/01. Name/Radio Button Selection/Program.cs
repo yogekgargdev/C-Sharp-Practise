67c511b1894420990031e0abe8f2d477ed82f962
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2022-10-06 15:08:14 +0530

    Radio Button Selection

---
+++ SeleniumWebDriver/NameSelector/01. Name/Radio Button Selection/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class EntryPoint
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement element; //-> not initialsing bcoz we have still not put the url string will throw exception
+    static void Main()
+    {
+        String url = "https://demo.guru99.com/test/radio.html";
+        String xpath = "";
+        String[] checkboxarray = { "1", "2", "3" };
+
+        driver.Navigate().GoToUrl(url);
+
+        xpath = "/html[1]/body[1]/div[4]/input[" + 3 + "]";
+        element = driver.FindElement(By.XPath(xpath));
+        element.Click();
+        Thread.Sleep(3000);
+
+
+
+        for (int i = 0; i < checkboxarray.Length; i++)
+        {
+            xpath = "/html[1]/body[1]/div[4]/input[" + checkboxarray[i] + "]";
+            element = driver.FindElement(By.XPath(xpath)); //-> This element is basically the radio box
+
+            if (element.GetAttribute("checked") == "true")
+            {
+                Console.WriteLine("The element at " + (i + 1) + " is checked!");
+            }
+            else
+            {
+                Console.WriteLine("The element at " + (i + 1) + " is not checked!");
+            }
+
+        }
+
+
+        driver.Quit();
+
+
+    }
+}
