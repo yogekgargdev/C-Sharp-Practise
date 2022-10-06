c6faf670466f42d9e06e2ad8a47d45e31d6df79b
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2022-10-06 10:47:20 +0530

    Name Selector Selenium

---
+++ SeleniumWebDriver/NameSelector/01. Name/01. Name/Program.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.google.com");
+
+        IWebElement element = driver.FindElement(By.Name("q")); //-> This element is basically the input box for Search
+
+        if (element.Displayed)
+        {
+            GreenMessage("The element is perfectly displayed!");
+        }
+        else
+        {
+            RedMessage("The element is not displayed, please check the code!");
+
+        }
+
+
+    }
+
+    private static void RedMessage(string v)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(v);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string v)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(v);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
