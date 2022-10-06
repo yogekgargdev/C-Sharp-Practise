a657119442f48125e7cc70b835a39489b8e96657
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2022-10-06 11:22:09 +0530

    ClassName Selector

---
+++ SeleniumWebDriver/NameSelector/01. Name/03. Class Selecctor/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        String url = "https://blog.udemy.com/c-sharp-projects/";
+        String Class_Name = "featured-article__title";
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement element = driver.FindElement(By.ClassName(Class_Name)); //-> This element is basically the top heading
+
+        if (element.Displayed)
+        {
+            GreenMessage("The element is perfectly displayed!");
+            Console.WriteLine("Text inside ClassName: "+ element.Text);
+        }
+        else
+        {
+            RedMessage("The element is not displayed, please check the code!");
+
+        }
+
+        driver.Quit();
+
+
+    }
+
+    private static void RedMessage(string v)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(v);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string v)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(v);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
