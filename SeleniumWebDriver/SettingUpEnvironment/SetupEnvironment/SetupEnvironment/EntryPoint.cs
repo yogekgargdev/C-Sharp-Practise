bf5f58f3c9c1f60a1c09de61b2455f51b62d073f
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2022-10-06 10:21:29 +0530

    Setting Up Basic Environent & Downlaoding NuGet Packages

--- SeleniumWebDriver/SettingUpEnvironment/SetupEnvironment/SetupEnvironment/EntryPoint.cs
+++ SeleniumWebDriver/SettingUpEnvironment/SetupEnvironment/SetupEnvironment/EntryPoint.cs
@@ -8,6 +8,7 @@ class EntryPoint
         IWebDriver driver = new ChromeDriver();
         driver.Navigate().GoToUrl("https://www.google.com");
         Thread.Sleep(3000); //-> 3 second thread sleep
+
         driver.Quit();
 
     }
